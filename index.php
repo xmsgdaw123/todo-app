bd23ec71a87d7413cefecee72eb3b0438fb8de5e
xmsgdaw123 <m.sepulveda1dawnuria2020@gmail.com>
2022-04-25 19:53:35 +0200

    feat: first version

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo App</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="/">Todo App</a>
+        </div>
+        <nav>
+            <a href="/">Inicio</a>
+            <a href="/">Login</a>
+        </nav>
+    </header>
+    <main>
+        <div class="section">
+            <div class="section-title">Todo App</div>
+            <div class="section-description">Crea tus tareas de tu día a día</div>
+            <div class="login-container">
+                <div class="login-title">Iniciar sesión</div>
+                <div class="input-box">
+                    <div class="input-title">Correo electrónico</div>
+                    <input class="input" type="text" placeholder="Correo...">
+                </div>
+                <div class="input-box">
+                    <div class="input-title">Contraseña</div>
+                    <input class="input" type="password" placeholder="Contraseña...">
+                    <div class="login-text">Crear una cuenta</div>
+                </div>
+                <div class="input-box">
+                    <button class="login-btn">Iniciar sesión</button>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>&copy; 2022 - Todo App</footer>
+</body>
+</html>
